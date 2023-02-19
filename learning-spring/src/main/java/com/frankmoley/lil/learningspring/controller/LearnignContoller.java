c9792ab0bfb222a2d5937b075051935f2bce5c64
Jim-John <jimjohn83@gmail.com>
2023-02-19 07:27:21 +0530

    Adding Contoller to Main Branch

---
+++ learning-spring/src/main/java/com/frankmoley/lil/learningspring/controller/LearnignContoller.java
@@ -0,0 +1,12 @@
+package com.frankmoley.lil.learningspring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/spanish-greetings")
+public class LearnignContoller {
+
+
+}

b358c611b5a72e77d285f34d70bbbe2b91de87a3
Jim-John-1 <jimjohn83@gmail.com>
2023-03-04 19:54:02 +0530

    Addign test_mainBranchChange in main branch

--- learning-spring/src/main/java/com/frankmoley/lil/learningspring/LearningSpringApplication.java
+++ learning-spring/src/main/java/com/frankmoley/lil/learningspring/LearningSpringApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearningSpringApplication {
-
+    private String firstName;
 	public static void main(String[] args) {
 		SpringApplication.run(LearningSpringApplication.class, args);
+		LearningSpringApplication learningSpringApplication=new  LearningSpringApplication();
+		learningSpringApplication.testBranchoneChange();
 	}
 
 	void test_mainBranchChange ()
